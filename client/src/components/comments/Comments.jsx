4d35b8231fdce133d53efc16d3bc64e6b2a86aae
Sabreena Khan <sabreenakhan945@gmail.com>
2026-04-20 13:27:24 +0530

    Fix: converted client submodule to regular folder

---
+++ client/src/components/comments/Comments.jsx
@@ -0,0 +1,71 @@
+import { useContext, useState } from "react";
+import "./comments.scss";
+import { AuthContext } from "../../context/authContext";
+import { useQuery, useMutation, useQueryClient } from '@tanstack/react-query';
+import { makeRequest } from "../../axios";
+import moment from "moment";
+
+const Comments = ({ postId }) => {
+  const [desc, setDesc] = useState("");
+  const { currentUser } = useContext(AuthContext);
+  const queryClient = useQueryClient();
+
+  // 1. Fetch Comments
+  const { isLoading, error, data } = useQuery({
+    queryKey: ["comments", postId],
+    queryFn: () =>
+      makeRequest.get("/comments?postId=" + postId).then((res) => {
+        return res.data;
+      }),
+  });
+
+  // 2. Mutation for adding a comment
+  const mutation = useMutation({
+    mutationFn: (newComment) => {
+      return makeRequest.post("/comments", newComment);
+    },
+    onSuccess: () => {
+      // Invalidate and refetch specific post comments
+      queryClient.invalidateQueries({ queryKey: ["comments", postId] });
+    },
+  });
+
+  const handleClick = async (e) => {
+    e.preventDefault();
+    mutation.mutate({desc, postId});
+    setDesc("");
+  };
+
+  return (
+    <div className="comments">
+      <div className="write">
+        <img src={currentUser.profilePic} alt="" />
+        <input
+          type="text"
+          placeholder="write a comment"
+          value={desc}
+          onChange={(e) => setDesc(e.target.value)}
+        />
+        <button onClick={handleClick}>Send</button>
+      </div>
+      {error
+        ? "Something went wrong"
+        : isLoading
+        ? "loading"
+        : data.map((comment) => (
+            <div className="comment" key={comment.id}>
+              <img src={comment.profilePic} alt="" /> {/* Fixed property name */}
+              <div className="info">
+                <span>{comment.name}</span>
+                <p>{comment.desc}</p>
+              </div>
+              <span className="date">
+                {moment(comment.createdAt).fromNow()} {/* Fixed typo: fromNow */}
+              </span>
+            </div>
+          ))}
+    </div>
+  );
+};
+
+export default Comments;
---
+++ client/src/components/posts/Posts.jsx
@@ -0,0 +1,25 @@
+import { makeRequest } from "../../axios";
+import Post from "../post/Post";
+import "./posts.scss";
+import {useQuery} from '@tanstack/react-query'
+
+const Posts = ({userId}) => {
+  //TEMPORARY
+  const { isLoading, error, data } = useQuery({
+    queryKey: ['posts'],
+    queryFn: () =>
+     makeRequest.get("/posts?userId="+userId).then(res=>{
+      return res.data;
+     })
+  });
+
+
+ 
+  return <div className="posts">
+    {error ? "Something went wrong ": (isLoading ? "Loading": data.map(post=>(
+      <Post post={post} key={post.id}/>
+    )))}
+  </div>;
+};
+
+export default Posts;
